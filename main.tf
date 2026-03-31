541c50c5f45cb8b8aab7617094251290ac41100e
Cristian Beraha <cristian@cloudops.works>
2026-03-31 18:12:44 -0300

    chore: Simplified WAFv2 metric naming in visibility configurations
    Updated `metric_name` to use a streamlined naming approach by removing vendor-specific prefixes, ensuring consistency and reducing unnecessary complexity.

--- main.tf
+++ main.tf
@@ -743,7 +743,7 @@ resource "aws_wafv2_web_acl_rule" "rule_group_references" {
 
   visibility_config {
     cloudwatch_metrics_enabled = try(each.value.visibility_config.cloudwatch_metrics_enabled, true)
-    metric_name                = try(each.value.visibility_config.metric_name, format("%s-%s", try(each.value.vendor_name, "AWS"), each.value.name))
+    metric_name                = try(each.value.visibility_config.metric_name, each.value.name)
     sampled_requests_enabled   = try(each.value.visibility_config.sampled_requests_enabled, true)
   }
 }
